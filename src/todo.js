b4074e0e9ebea4767aa095edbf5657565d13e97d
Scott E Thompson <sthompson082290@gmail.com>
2024-08-12 10:02:21 -0500

    Initial commit

---
+++ src/todo.js
@@ -0,0 +1,10 @@
+// Given a user wants to make a prayer request list. 
+// When the user wants to update the list; add prayer request to a list, read the list, edit the items, delete the items
+// Then the user will check the list and make changes as necessary
+
+// TODO - 0) Setup CI/CD, host on Netlify
+// TODO - 1) Make a mock list and render it on the main page
+// TODO - 2) Include user ability to add items
+// TODO - 3) Include user ability to edit items
+// TODO - 4) Include user ability to delete items
+// TODO - 5) Include user ability to change item order
